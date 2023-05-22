085d9742d590de70b2d198a1dfbb1dc955de2e39
Augusto Melillo <114231094+AugustoMF@users.noreply.github.com>
2023-05-22 14:51:13 -0300

    exercise.py

--- exercise.py
+++ exercise.py
@@ -1,12 +1,13 @@
-weight =  int (input ("How much do you weight? "))
-question = input ("Type L por lbs or K to kilos ")
+weight = int(input("Insert your weight "))
+unit = input ("insert the measurement unit (K fot kgs or L for lbs) ")
 
-if question.upper () == "K":
+if unit.upper () == "K":
     converted = weight / 0.45
-    print ("Weight in kilos " + str(converted))
+    print ("Weight in pounds " + str(converted))
 
 else:
-     converted = weight * 0.45
-     print ("Weight in pounds " + str(converted))
+    converted = weight * 0.45
+    print ("Weight in KGs " + str(converted))
+    
 
 print ("Done")
